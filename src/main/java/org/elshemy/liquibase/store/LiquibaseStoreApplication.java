3cc6626a1341aa18d94ba21f040f39cf2af59c61
fathy.elshemy <fathy.elshemy@d360.com>
2024-02-10 20:44:25 +0300

    build main project to try liquibase migration with spring boot

---
+++ src/main/java/org/elshemy/liquibase/store/LiquibaseStoreApplication.java
@@ -0,0 +1,13 @@
+package org.elshemy.liquibase.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elshemy/liquibase/store/LiquibaseStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elshemy.liquibase.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
